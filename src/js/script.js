5d398926682e896225b8911d39cd8d8daad16ec8
Mikhail Zakharov <mike.zakharov1990@gmail.com>
2021-04-15 18:06:32 +0300

    add modals

--- src/js/script.js
+++ src/js/script.js
@@ -37,4 +37,21 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('0.5s');    
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('0.5s');
+    });
+
+    $('.button_catalog-item').each(function(i){
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); 
+            $('.overlay, #order').fadeIn('0.5s');    
+        });
+    });
 });
